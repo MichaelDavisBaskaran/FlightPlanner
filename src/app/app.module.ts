099ff9f0374a1bea55ee8f640fb6001df5c0a27d
cbridel <cbridel@yahoo.ca>
2022-11-17 21:50:42 -0500

    Initial layout of filter menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import {Component} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import { MatMenuModule} from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { FirstComponent } from './first/first.component';
 
 @NgModule({
@@ -22,7 +23,8 @@ import { FirstComponent } from './first/first.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatMenuModule
+    MatMenuModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
